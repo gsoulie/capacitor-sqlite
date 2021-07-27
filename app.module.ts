eb9e4ce548469a01ec1f7739865b41e7c2690f21
gsoulie <gsoulie@users.noreply.github.com>
2021-07-27 11:05:58 +0200

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { SQLiteService } from './shared/services/sqlite/sqlite.service';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    HttpClientModule,
+    AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SQLiteService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
