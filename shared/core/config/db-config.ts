9d2499e5b47a60fb4a01ab6a8874b9f5061d0405
gsoulie <gsoulie@users.noreply.github.com>
2021-07-27 10:57:22 +0200

    Create db-config.ts

---
+++ shared/core/config/db-config.ts
@@ -0,0 +1,14 @@
+export const dbConfig = {
+  dbName: 'testEncryption',
+  encrypted: false,
+  mode: 'no-encryption',
+  version: 1
+};
+
+export const dbConfigWithEncryption = {
+  dbName: 'testEncryption',
+  encrypted: true,
+  mode: 'encryption',
+  version: 1
+};
+
